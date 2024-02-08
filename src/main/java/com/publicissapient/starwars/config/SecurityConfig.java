b33c870549efc730e35119a1b136a287e6b9fde8
Prathamesh Jadhav <prathameshj610@gmail.com>
2024-02-08 14:11:26 +0530

    Changed security config and added Jenkins script file

--- src/main/java/com/publicissapient/starwars/config/SecurityConfig.java
+++ src/main/java/com/publicissapient/starwars/config/SecurityConfig.java
@@ -1,22 +1,22 @@
-//package com.publicissapient.starwars.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//@Configuration
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
-//                        .authenticated())
-//                .httpBasic(withDefaults())
-//                .formLogin(withDefaults())
-//                .csrf(AbstractHttpConfigurer::disable);
-//        return http.build();
-//    }
-//}
+package com.publicissapient.starwars.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
+                        .authenticated())
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
